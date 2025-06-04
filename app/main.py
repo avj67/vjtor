2e838306773b98417d7a9561cd321723c2836031
Aaryan Vijai <aaryanvijai2005@gmail.com>
2025-06-04 12:19:41 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import sy
 import bencodepy
 import requests
+import sys
 
 
 
@@ -10,22 +11,38 @@ import requests
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
-    return bencodepy.decode(bencoded_value)
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index + 1 :]
-    elif chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1] == "e"):
-        return int(bencoded_value[1:-1])
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+    def extract_string(data):
+        length, rest = data.split(b":",1)
+        length = int(length)
+        return rest[:length], rest[length:]
+    # Recursive decode function
+    def decode(data):
+        if data[0:1].isdigit():# byte string like b"5:hello"
+            decoded_str, rest =  extract_string(data) 
+            return decoded_str, rest
+        elif data.startswith(b'i'): # integer like b'i42e'
+            end = data.index(b'e')
+            return int(data[1:end]), data[end+1:]
+        elif data.startswith(b'l'):
+            data = data[l:]
+            result = []
+            # Technically only recursive if we have nested lists
+            while not data.startswith(b'e'):
+                item, data = decode(data)
+                result.append(item)
+            return result, data[1:]
+        else:
+            raise ValueError("Unsupported or invalid bencoded value")
+    deecoded_value, _ = decode(bencoded_value)
+    return deecoded_value
         
+    
+
 def main():
     command = sys.argv[1]
 
     
-    print("Logs from your program will appear here!", file=sys.stderr)
+
 
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
