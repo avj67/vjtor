f4819352fc51fa2b2442de9ff7236d6fa67f6067
Aaryan Vijai <aaryanvijai2005@gmail.com>
2025-06-04 11:05:42 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def decode_bencode(bencoded_value):
 def main():
     command = sys.argv[1]
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    
     print("Logs from your program will appear here!", file=sys.stderr)
 
     if command == "decode":
@@ -29,16 +29,15 @@ def main():
 
         # json.dumps() can't handle bytes, but bencoded "strings" need to be
         # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
+        
         def bytes_to_str(data):
             if isinstance(data, bytes):
                 return data.decode()
 
             raise TypeError(f"Type not serializable: {type(data)}")
 
-        # Uncomment this block to pass the first stage
-        # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        
+        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
